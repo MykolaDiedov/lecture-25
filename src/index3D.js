9635f8189c35f9b62a631bd5173a0f3b7744fbda
MykolaDiedov <diedov.nn@gmail.com>
2021-03-22 09:31:02 +0200

    second commit

--- src/index3D.js
+++ src/index3D.js
@@ -17,10 +17,10 @@ const geometry = new THREE.TorusKnotGeometry( 10, 3, 100, 16 );
 const material = new THREE.MeshPhysicalMaterial({
     color: '#00FF00'
 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const knot = new THREE.Mesh(geometry, material);
+scene.add(knot);
 camera.position.z = 20;
-cube.rotation.y = 15;
+knot.rotation.y = 15;
 const light = new THREE.DirectionalLight('#fff', 0.7)
 scene.add(light);
 light.position.set(-1, 2, 4);
@@ -28,8 +28,8 @@ light.position.set(-1, 2, 4);
 function animate () {
     renderer.render(scene, camera);
     requestAnimationFrame(animate)
-    cube.rotation.z += 0.01;
-    cube.rotation.y += 0.01;
+    knot.rotation.z += 0.01;
+    knot.rotation.y += 0.01;
     controls.update();
 }
 animate();
